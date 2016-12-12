90ff264918d95c22e9e9eb6fb9dae6287c736d87
Jacob Timer <Jacob Timer>
2016-12-11 22:30:01 -0500

    Work

--- clashcard.js
+++ clashcard.js
@@ -1,35 +1,69 @@
 var fs = require("fs");
-var inquirer = require("inquirer")
+var inquirer = require("inquirer");
 
 function BasicFlashcard(front, back) {
 
     this.front = front;
     this.back = back;
-    this.upload = function() {
- fs.appendFileSync("flashcards.txt", "Question: " + this.front + "\nAnswer: " + this.back + "\n");
+    this.upload = function () {
+        fs.appendFileSync("flashcards.txt", "Basic\n Question: " + this.front + "\nAnswer: " + this.back + "\n");
     }
 
 }
 
+function ClozeFlashcard(cloze, question) {
+
+    this.question = question;
+    this.cloze = cloze;
+    this.upload = function () {
+        this.cloze = this
+            .cloze
+            .italics()
+        fs.appendFileSync("flashcards.txt", "Cloze\n Question: " + cloze + question + "\n");
+    }
+
+}
 
 function start() {
     inquirer.prompt([
         {
             type: "list",
             message: "Basic or Cloze?",
-            choices: ['Basic', 'Cloze' ],
-                name: "bOc",
-            
+            choices: [
+                'Basic', 'Cloze'
+            ],
+                name: "bOc"
             }
         ])
         .then(function (start) {
             if (start.bOc === 'Basic') {
                 createBasic()
+            } else if (start.bOc === 'Cloze') {
+                createCloze()
             }
 
         });
 }
 
+function again() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: "Make another card?",
+                name: "confirm",
+                default: true
+            }
+        ])
+        .then(function (again) {
+            if (again.confirm == true) {
+                start()
+            } else {
+                console.log("Thanks!")
+            }
+
+        });
+}
 
 function createBasic() {
 
@@ -37,20 +71,66 @@ function createBasic() {
         {
             type: "input",
             message: "What would you like the front to say?",
-            name: "front",
+            name: "front"
         }, {
                 type: "input",
                 message: "The Back?",
-                name: "back",
-
+                name: "back"
             }
         ])
         .then(function (response) {
             var newCard = new BasicFlashcard(response.front, response.back)
             newCard.upload();
-console.log("Thanks!")
+            again()
         });
 
+}
+
+function createCloze() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What's the question?",
+                name: "front"
+            }
+        ])
+        .then(function (response) {
+            var question = response.front;
+            var splitQuestion = question.split(" ");
+
+            inquirer
+                .prompt([
+                    {
+                        type: "checkbox",
+                        message: "Select the cloze.",
+                        choices: splitQuestion,
+                        name: "cloze"
+                    }
+                ])
+                .then(function (response) {
+                    cloze = response.cloze
+                    clozeLength = cloze.length
+
+                    
+                        for (i = 0; i < clozeLength; i++) {
+
+                        splitQuestion = splitQuestion.shift()
+
+                    }
+
+
+                    question = splitQuestion.join(" ")
+                    cloze = cloze.join(" ")
+
+                    var newCard = new ClozeFlashcard(cloze, question)
+
+                    newCard.upload();
+                    again()
+
+                });
+
+        });
 }
 
 start();
